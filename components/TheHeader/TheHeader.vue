c2fc7baf18b5a5c3e7a620c53467e3df6a5fb66e
mtotodev05 <mtotodev05@gmail.com>
2018-08-20 11:28:48 +0300

    Added the Header.vue

---
+++ components/TheHeader/TheHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="main-header">
+    <nav class="main-nav">
+      <ul class="nav-links">
+        <nuxt-link to="/" tag="li"><a>All Posts</a></nuxt-link>
+        <nuxt-link to="/about"tag="li"><a>About</a></nuxt-link>
+      </ul>
+    </nav>
+  </header>
+</template>
+<style scoped>
+
+  .main-header{
+    position:fixed;
+    top:0;
+    left:0;
+    width:100%;
+    background:#022d30
+
+  }
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
