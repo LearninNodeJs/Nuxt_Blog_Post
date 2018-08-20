dac23a374c29a415bec860b2d76956bd513e690d
mtotodev05 <mtotodev05@gmail.com>
2018-08-20 12:45:26 +0300

    Fixed bug in css code

--- components/TheHeader/TheHeader.vue
+++ components/TheHeader/TheHeader.vue
@@ -3,7 +3,7 @@
     <nav class="main-nav">
       <ul class="nav-links">
         <nuxt-link to="/" tag="li" class="nav-link"><a>All Posts</a></nuxt-link>
-        <nuxt-link to="/about"tag="li"><a>About</a></nuxt-link>
+        <nuxt-link to="/about"tag="li" class="nav-link"><a>About</a></nuxt-link>
       </ul>
     </nav>
   </header>
