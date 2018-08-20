2d601a8301e81ff9aa1cee0024191d4c25185545
mtotodev05 <mtotodev05@gmail.com>
2018-08-20 12:41:38 +0300

    Nav bars

--- components/TheHeader/TheHeader.vue
+++ components/TheHeader/TheHeader.vue
@@ -2,7 +2,7 @@
   <header class="main-header">
     <nav class="main-nav">
       <ul class="nav-links">
-        <nuxt-link to="/" tag="li"><a>All Posts</a></nuxt-link>
+        <nuxt-link to="/" tag="li" class="nav-link"><a>All Posts</a></nuxt-link>
         <nuxt-link to="/about"tag="li"><a>About</a></nuxt-link>
       </ul>
     </nav>
@@ -15,7 +15,42 @@
     top:0;
     left:0;
     width:100%;
-    background:#022d30
+    height:4.5rem;
+    background:#022d30;
+
+  }
+  .main-nav{
+    height:100%;
+  }
+  .nav-links {
+    list-style: none;
+    margin:0;
+    padding:0;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    height:100%;
+  }
+  .nav-link{
+    height:100%;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    margin:0 1rem;
+    padding:0.3rem;
+  }
+  .nav-link.nuxt-link-exact-active{
+    border-bottom: 3px solid #06c4d1;
+  }
+  .nav-link a{
+    display:block;
+    text-decoration: none;
+    color:white;
+  }
+  .nav-link a:hover,
+  .nav-link a:active,
+  .nav-link.nuxt-link-exact-active a{
+    color:#06c4d1;
 
   }
 
