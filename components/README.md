deac3ffb3d3baa48eee7724de8097089fea538ac
mtotodev05 <mtotodev05@gmail.com>
2018-08-21 10:43:28 +0300

    Implemented blog post with components

--- components/README.md
+++ components/README.md
@@ -5,3 +5,26 @@
 The components directory contains your Vue.js Components.
 
 _Nuxt.js doesn't supercharge these components._
+
+`<script>`
+   `export default{
+     props:{
+       title:{
+         type:String,
+         required:true
+       },**dddsdswdw**
+       excerpt:{
+         type:String,
+         required:true
+       },
+       thumbnailImage:{
+         type:String,
+         required:true
+       },
+       id:{
+         type:String,
+         required:true
+       }
+     }
+   }`
+` </script>`
--- components/TheHeader/TheHeader.vue
+++ components/TheHeader/TheHeader.vue
@@ -9,63 +9,45 @@
   </header>
 </template>
 <style scoped>
-
   .main-header{
-    position:fixed;
-    top:0;
-    left:0;
-    width:100%;
-    height:4.5rem;
-    background:#022d30;
-
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 4.5rem;
+    background: #022d30;
   }
   .main-nav{
-    height:100%;
+    height: 100%;
   }
   .nav-links {
     list-style: none;
-    margin:0;
-    padding:0;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    height:100%;
-  }
-  .nav-link{
-    height:100%;
-    display:flex;
+    margin: 0;
+    padding: 0;
+    display: flex;
     justify-content: center;
     align-items: center;
-    margin:0 1rem;
-    padding:0.3rem;
+    height: 100%;
+  }
+  .nav-link{
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 1rem;
+    padding: 0.3rem;
   }
   .nav-link.nuxt-link-exact-active{
     border-bottom: 3px solid #06c4d1;
   }
   .nav-link a{
-    display:block;
+    display: block;
     text-decoration: none;
-    color:white;
+    color: white;
   }
   .nav-link a:hover,
   .nav-link a:active,
   .nav-link.nuxt-link-exact-active a{
-    color:#06c4d1;
-
+    color: #06c4d1;
   }
-
 </style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
